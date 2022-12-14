bcf0a004a93b58b5e9926dc26f754a18b3e308ba
Tiques1 <tiques13@mail.ru>
2022-12-15 03:06:03 +0500

    1Task complete

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace Numbers
     {
         public static void Main()
         {
-
+            IntOrFloat(Console.ReadLine());
+        }
+        private static void IntOrFloat(string num)
+        {
+            float lastnum = 0;
+            while (num != "q")
+            {            
+                if (int.TryParse(num, out int number))
+                {
+                    Console.WriteLine((char)number);
+                }
+                else if (float.TryParse(num, out float floatnumber))
+                {                    
+                    if (floatnumber == lastnum)
+                        break;
+                    lastnum = float.Parse(num);
+                }
+                num = Console.ReadLine();
+            }
         }
     }
 }
