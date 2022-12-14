c9a3a9ba492c8db3a304b4fff0de620fb617cf76
Tiques1 <tiques13@mail.ru>
2022-12-15 03:30:58 +0500

    SecondTask complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 
 namespace Numbers
 {
@@ -6,7 +7,25 @@ namespace Numbers
     {
         public static void Main()
         {
-
+            Console.WriteLine(CheckOfCorrect(Console.ReadLine()));
+        }
+        public static object CheckOfCorrect(string number)
+        {
+            int counter = 0;
+            string listofnum = "0123456789-";
+            foreach(char c in number)
+            {
+                if (!listofnum.Contains(c))
+                    return "Не целочисленное!";
+                for (int i = 0; i < 10; i++)
+                {
+                    if (listofnum[i] == c)
+                    {
+                        counter += i;
+                    }
+                }
+            }
+            return counter;
         }
     }
 }
